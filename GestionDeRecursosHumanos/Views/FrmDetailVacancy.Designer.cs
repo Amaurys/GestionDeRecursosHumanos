db7bbd146251f7c51f63b5777017404d5d42d03d
amsanchez <amsanchez@28FZCH2>
2018-04-13 17:01:32 -0400

    frmDetailVacancy updated.

--- GestionDeRecursosHumanos/Views/FrmDetailVacancy.Designer.cs
+++ GestionDeRecursosHumanos/Views/FrmDetailVacancy.Designer.cs
@@ -30,57 +30,119 @@
         {
             this.lblNombreVacante = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.lblDepartment = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.lblVacancyDetail = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblNombreVacante
             // 
             this.lblNombreVacante.AutoSize = true;
-            this.lblNombreVacante.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombreVacante.Location = new System.Drawing.Point(141, 9);
+            this.lblNombreVacante.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.lblNombreVacante.Location = new System.Drawing.Point(47, 34);
             this.lblNombreVacante.Name = "lblNombreVacante";
-            this.lblNombreVacante.Size = new System.Drawing.Size(208, 55);
+            this.lblNombreVacante.Size = new System.Drawing.Size(67, 17);
             this.lblNombreVacante.TabIndex = 1;
             this.lblNombreVacante.Text = "Vacante";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(94, 101);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(47, 95);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.Size = new System.Drawing.Size(94, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Departamento: ";
-            // 
-            // lblDepartment
-            // 
-            this.lblDepartment.AutoSize = true;
-            this.lblDepartment.Location = new System.Drawing.Point(180, 101);
-            this.lblDepartment.Name = "lblDepartment";
-            this.lblDepartment.Size = new System.Drawing.Size(72, 13);
-            this.lblDepartment.TabIndex = 3;
-            this.lblDepartment.Text = "lblDepartment";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(94, 140);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(47, 150);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(116, 13);
+            this.label3.Size = new System.Drawing.Size(139, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Detalles de la vacante:";
             // 
-            // lblVacancyDetail
+            // richTextBox1
             // 
-            this.lblVacancyDetail.AutoEllipsis = true;
-            this.lblVacancyDetail.AutoSize = true;
-            this.lblVacancyDetail.Location = new System.Drawing.Point(94, 178);
-            this.lblVacancyDetail.Name = "lblVacancyDetail";
-            this.lblVacancyDetail.Size = new System.Drawing.Size(86, 13);
-            this.lblVacancyDetail.TabIndex = 5;
-            this.lblVacancyDetail.Text = "lblVacancyDetail";
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(50, 176);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(330, 75);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(147, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(115, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(47, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Fecha vacante:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(147, 304);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(115, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(147, 353);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(115, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(47, 353);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Estado:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(50, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 53);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Aplicar a esta vacante";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(287, 427);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 53);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Volver Atrás";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // FrmDetailVacancy
             // 
@@ -88,13 +150,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblVacancyDetail);
+            this.ClientSize = new System.Drawing.Size(428, 519);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.lblDepartment);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblNombreVacante);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FrmDetailVacancy";
             this.Text = "Detalle de Vacante";
             this.ResumeLayout(false);
@@ -106,8 +176,14 @@
 
         private System.Windows.Forms.Label lblNombreVacante;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblDepartment;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label lblVacancyDetail;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
